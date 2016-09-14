c2612e7fecf8f7b78592452472640d7545dc8d04
Radim Hopp <rhopp@redhat.com>
2016-09-14 14:06:27 +0200

    New tests for Docker: Docker Compose Docker Launch Configuration

---
+++ plugins/org.jboss.tools.docker.reddeer/src/org/jboss/tools/docker/reddeer/preferences/DockerComposePreferencePage.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.docker.reddeer.preferences;
+
+import org.jboss.reddeer.jface.preference.PreferencePage;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+
+/**
+ * 
+ * 
+ * @author jkopriva
+ */
+
+public class DockerComposePreferencePage extends PreferencePage {
+
+	public DockerComposePreferencePage() {
+		super("Docker", "Docker Compose");
+	}
+
+	public void setPathToDockerCompose(String path) {
+		new LabeledText("Docker Compose ").setText(path);
+	}
+}
