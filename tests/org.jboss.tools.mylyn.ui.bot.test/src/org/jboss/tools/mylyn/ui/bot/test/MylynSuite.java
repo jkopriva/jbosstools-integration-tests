8681e8f48e710178ee42fca7621c43e2498f9600
jkopriva <jkopriva@redhat.com>
2017-04-28 07:34:07 +0200

    JBIDE-24371 Mylyn refactoring for RedDeer2

--- tests/org.jboss.tools.mylyn.ui.bot.test/src/org/jboss/tools/mylyn/ui/bot/test/MylynSuite.java
+++ tests/org.jboss.tools.mylyn.ui.bot.test/src/org/jboss/tools/mylyn/ui/bot/test/MylynSuite.java
@@ -1,10 +1,9 @@
 package org.jboss.tools.mylyn.ui.bot.test;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import org.jboss.tools.mylyn.ui.bot.test.MylynTestJenkins;
 
 /**
  * Mylyn Test Suite
@@ -13,8 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 
 @RunWith(RedDeerSuite.class)
-
-@SuiteClasses({
+@Suite.SuiteClasses({
 	MylynTestJenkins.class,
 	MylynTestValidate.class,
 	MylynTestLocalRepo.class,
